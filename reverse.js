dd052769f9d5a31c160576a16946b6ad935a30f4
Yarmoli4 <70720360+Yarmoli4@users.noreply.github.com>
2020-10-20 02:02:32 +0300

    Create reverse.js

---
+++ reverse.js
@@ -0,0 +1,11 @@
+function reverse (n) {
+    return (
+        parseFloat(
+          n
+            .toString()
+            .split('')
+            .reverse()
+            .join('')
+        ) 
+      )  
+        }     
